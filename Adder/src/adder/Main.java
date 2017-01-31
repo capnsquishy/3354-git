d377d28d572f85c65466572b9ccde1df646fda4a
capnsquishy <kramsnilloc@gmail.com>
2017-01-31 00:31:02 -0600

    Revert "Changes made to main method"
    This reverts commit f48ada074a9978179aa9ae930b40185768419c3b.

--- Adder/src/adder/Main.java
+++ Adder/src/adder/Main.java
@@ -7,12 +7,7 @@ public class Main {
             int result = addArguments(args);
             System.out.println(result);
         } catch (Exception e) {
-            if(!Character.isDigit(args[0]) && !args[0].equals("-"))
-                System.err.println("Please provide only integers to add");
-            else if(args.length < 2)
-                System.err.println("Not enough arguments were input. Please enter two or more integers to add, or use \"-\" as the first argument.");
-            else
-                System.err.println("Be sure that all arguments are integers, or the first argument is a \"-\"");
+            System.err.println("Please provide only integers to add");
         }
     }
 
