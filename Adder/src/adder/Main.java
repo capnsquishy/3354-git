6962a4bc8ae76c341ed148480729ec3ec2c48e36
capnsquishy <kramsnilloc@gmail.com>
2017-01-31 00:09:51 -0600

    Changes made to Main.java
    The addArguments method was modified to add an arbitrary number of
    positive integers instead of just two, as well as the error message to
    fit the change.

--- Adder/src/adder/Main.java
+++ Adder/src/adder/Main.java
@@ -7,11 +7,17 @@ public class Main {
             int result = addArguments(args);
             System.out.println(result);
         } catch (Exception e) {
-            System.err.println("Please provide two integers to add");
+            System.err.println("Please provide only integers to add");
         }
     }
 
     private static int addArguments(String[] args) {
-        return Integer.valueOf(args[0]) + Integer.valueOf(args[1]);
+        int sum = 0;
+        for(int i = 0; i < args.length - 1; i++)
+        {
+            sum += Integer.valueOf(args[i]);
+        }
+
+        return sum;
     }
 }
